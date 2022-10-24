c920f2a7d1fd5b277e19b243d9d1b7b90527c8b8
andreadldev <andrea.delisi2@gmail.com>
2022-10-24 16:43:56 +0200

    Mostra risultati nell'html

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+const container = document.querySelector('.container');
+
 for (let i = 1; i <= 100; i++){
-    if (i % 3 === 0 && i % 5 ===0) console.log("Fizzbuzz");
-    else if (i % 3 === 0) console.log("Fizz");
-    else if (i % 5 === 0) console.log("Buzz");
-    else console.log(i);
+    if (i % 3 === 0 && i % 5 ===0) 
+        console.log("Fizzbuzz"),
+        container.append("Fizzbuzz");
+
+    else if (i % 3 === 0) 
+        console.log("Fizz"),
+        container.append("Fizz");
+
+    else if (i % 5 === 0) 
+        console.log("Buzz"),
+        container.append("Buzz");
+    else 
+        console.log(i)
+        container.append(i);
 }
